e2d31fc916cc8aac8c3928e56bd57c6444d96cc9
Darya <45762789+DaryaYa@users.noreply.github.com>
2021-10-28 14:04:43 +0300

    feat: add form

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,28 @@
 import '../scss/app.scss';
 
+const $ = require('jquery');
+
+$(document).ready(function() {
+  // E-mail Ajax Send
+  $('form').submit(function() { // Change
+    var th = $(this);
+    $.ajax({
+      type: 'POST',
+      url: 'mail.php', // Change
+      data: th.serialize(),
+    }).done(function() {
+      // eslint-disable-next-line no-alert
+      // eslint-disable-next-line no-undef
+      alert('Thank you!');
+      setTimeout(function() {
+        // Done Functions
+        th.trigger('reset');
+      }, 1000);
+    });
+    return false;
+  });
+});
+
 /* Your JS Code goes here */
 
 /* Demo JS */
